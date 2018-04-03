6c95260e06e7fbaaf221d3db35df48c4231c6d01
Himanshu Neema <himanshun.iitkgp@gmail.com>
2018-04-03 01:42:02 -0700

    container names

--- certbot.sh
+++ certbot.sh
@@ -3,8 +3,8 @@
 # RUN THIS AFTER docker-compose, so that nginx container is up
 source .env
 
-docker exec  ghostblog_nginx_1 /bin/bash -c "mkdir -p /etc/apt/sources.list.d/ && touch /etc/apt/sources.list.d/backports.list"
-docker exec  ghostblog_nginx_1 /bin/bash -c " echo 'deb http://ftp.debian.org/debian stretch-backports main' > /etc/apt/sources.list.d/backports.list"
-docker exec ghostblog_nginx_1 apt-get update
-docker exec ghostblog_nginx_1 apt-get install python-certbot-nginx -t stretch-backports -y
-docker exec -it ghostblog_nginx_1 certbot --nginx 
+docker exec  nginx_container /bin/bash -c "mkdir -p /etc/apt/sources.list.d/ && touch /etc/apt/sources.list.d/backports.list"
+docker exec  nginx_container /bin/bash -c " echo 'deb http://ftp.debian.org/debian stretch-backports main' > /etc/apt/sources.list.d/backports.list"
+docker exec nginx_container apt-get update
+docker exec nginx_container apt-get install python-certbot-nginx -t stretch-backports -y
+docker exec -it nginx_container certbot --nginx 
